2d0ee3f0ad210b8555ad01ebb676ea2cfddfe0b6
sumusa <physiosummy@gmail.com>
2024-04-02 16:13:30 -0230

    update navigation toggle

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const header = document.querySelector(".navbar")
+
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav-link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
